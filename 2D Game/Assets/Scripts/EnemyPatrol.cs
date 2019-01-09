7083e9d4abd362113336afd056c0f4bbdc7dae44
Unknown <sabrina.p338@stu.nebo.edu>
2019-01-09 10:27:57 -0700

    Turned in final

--- 2D Game/Assets/Scripts/EnemyPatrol.cs
+++ 2D Game/Assets/Scripts/EnemyPatrol.cs
@@ -29,11 +29,11 @@ public class EnemyPatrol : MonoBehaviour {
 		}
 
 		if (MoveRight){
-			transform.localScale = new Vector3(-0.2f,0.2f,1f);
+			transform.localScale = new Vector3(-4f,4f,1f);
 			GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
 		}
 		else {
-			transform.localScale = new Vector3(0.2f,0.2f,1f);
+			transform.localScale = new Vector3(4f,4f,1f);
 			GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
 		}
 	}
